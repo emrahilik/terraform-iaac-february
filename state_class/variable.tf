cc4b4d957586d86c2da68bf57bfd13783c2cb090
EC2 Default User <ec2-user@ip-172-31-30-251.eu-west-2.compute.internal>
2020-05-11 23:01:07 +0000

    SDLC

--- state_class/variable.tf
+++ state_class/variable.tf
@@ -1,7 +1,13 @@
-variable "vpc_cidr" {}
-
+variable "environment" {} 
+variable "s3_bucket" {} 
+variable "s3_folder_project" {} 
+variable "s3_folder_region" {} 
+variable "s3_folder_type" {} 
+variable "s3_tfstate_file" {} 
 variable "region" {}
-
-variable "private_subnet1" {}
-
-
+variable "public_cidr1" {}
+variable "public_cidr2" {}
+variable "public_cidr3" {}
+variable "private_cidr1" {}
+variable "private_cidr2" {}
+variable "private_cidr3" {}
